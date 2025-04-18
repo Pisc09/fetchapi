a11ad2ae9d9c9f4338a481a6ccf588ec189de5c9
Pisc09 <pisco8118@gmail.com>
2025-04-18 16:28:47 +0200

    code mis à jour

--- scripts.js
+++ scripts.js
@@ -1,26 +1,50 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 console.log(BASE_URL);
 
-console.log(table);
+const tbody = document.querySelector("tbody");
+console.log(tbody);
 
 async function callApi() {
-  const response = await fetch(BASE_URL + "users");
+  const response = await fetch(BASE_URL + "users/");
   const data = await response.json();
   console.log(data);
 
+  let users = data;
+  console.log(users);
+
+  showData(users);
   return data;
 }
-callApi();
 
-async function showData() {
+function showData(users) {
+  console.log(users);
   for (let i = 0; i < users.length; i++) {
-    const users = await callApi();
-    const table = document.querySelectorAll("td");
-    table[0].textContent = users.name;
-    table[1].textContent = users.email;
-    table[2].textContent = users.phone;
-    table[3].textContent = users.website;
-    table[4].textContent = users.address.zipcode;
+    console.log(users[i]);
+
+    let tr = document.createElement("tr");
+
+    const tdName = document.createElement("td");
+    const tdEmail = document.createElement("td");
+    const tdPhone = document.createElement("td");
+    const tdWebsite = document.createElement("td");
+    const tdZipCode = document.createElement("td");
+
+    tdName.innerText = users[i].name;
+    tr.appendChild(tdName);
+
+    tdEmail.innerText = users[i].email.toLowerCase();
+    tr.appendChild(tdEmail);
+
+    tdPhone.innerText = users[i].phone;
+    tr.appendChild(tdPhone);
+
+    tdWebsite.innerText = users[i].website;
+    tr.appendChild(tdWebsite);
+
+    tdZipCode.innerText = users[i].address.zipcode;
+    tr.appendChild(tdZipCode);
+
+    tbody.appendChild(tr);
   }
 }
-showData();
+callApi();
