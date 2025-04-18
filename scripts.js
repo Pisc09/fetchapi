b9bd264d5ebf254226f7c126ba37dc1f8fbf3a7b
Pisc09 <pisco8118@gmail.com>
2025-04-18 11:15:12 +0200

    souci "token" ? ou syntaxe pas bonne

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ const BASE_URL = fetch("https://jsonplaceholder.typicode.com/");
 async function nameApi() {
   fetch(BASE_URL + "todos/");
   const response = await BASE_URL;
-  const data = response;
+  const data = response.json();
   console.log(data);
+  return data;
 }
 nameApi();
