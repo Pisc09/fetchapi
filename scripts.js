113dc1ee5c9c7b220ef89525ecec69011e9e617a
Pisc09 <pisco8118@gmail.com>
2025-04-18 14:58:18 +0200

    avec for

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 console.log(BASE_URL);
 
-let usersName;
-let showUsers;
-const h2 = document.querySelector("h2");
+console.log(table);
 
 async function callApi() {
-  const response = await fetch(BASE_URL + "users/1");
+  const response = await fetch(BASE_URL + "users");
   const data = await response.json();
-  //   console.log(data);
-  console.log("test");
+  console.log(data);
 
-  usersName = data;
-  console.log(usersName);
-  return usersName;
+  return data;
 }
 callApi();
 
-callApi().then((usersName) => {
-  showUsers = usersName;
-  console.log(showUsers);
-  h2.append(usersName.name);
-});
+async function showData() {
+  for (let i = 0; i < users.length; i++) {
+    const users = await callApi();
+    const table = document.querySelectorAll("td");
+    table[0].textContent = users.name;
+    table[1].textContent = users.email;
+    table[2].textContent = users.phone;
+    table[3].textContent = users.website;
+    table[4].textContent = users.address.zipcode;
+  }
+}
+showData();
